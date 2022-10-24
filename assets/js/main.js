235e537155165faa5a4998ca0d9baf5587c27d17
vanelnw <vanelnwaba@gmail.com>
2022-10-24 21:35:50 +0100

    Add books list dynamicaly using javascript and add the remove button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-const awesomeBooks = [];
+let awesomeBooks = [];
 let id = 0
 
 if (awesomeBooks.length > 0) {
@@ -12,11 +12,35 @@ function addBook(title, author) {
   newBook.title = title;
   newBook.author = author;
   awesomeBooks.push(newBook);
+
+
 }
 
 const addButton = document.querySelector("#add-button");
 addButton.addEventListener('click', () => {
   addBook(document.querySelector("#title").value, document.querySelector("#author").value);
   console.log(awesomeBooks)
+})
+
+
+const books = document.querySelector('.books-content');
+
+function removeBook(id) {
+  awesomeBooks = awesomeBooks.filter((book) => book.id != id);
+  books.querySelector(`#Book${id}`).remove();
+}
+
+awesomeBooks.forEach((book) => {
+  books.innerHTML += `
+  <div class="single-book" id="Book${book.id}">
+      <div class="title">${book.title}</div>
+      <div class="author">${book.author}</div>
+      <button>remove</button>
+    </div>
+  `;
+  const Button = document.querySelector("button");
+  Button.addEventListener('click', () => {
+    removeBook(book.id);
+  })
 })
 
