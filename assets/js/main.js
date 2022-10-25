2703506898e4b6ef032c6ef3999036c8c6fab9e7
Luis Muñoz <lmunozm1702@gmail.com>
2022-10-25 16:52:16 -0300

    Refactor Add and Remove functions with classes.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,63 @@
 const books = document.querySelector('.books-content');
 
+function deleteBook(id) {
+  const [a, b] = id.split('_');
+  myLibrary.removeBook(b);
+  console.log(myLibrary);
+  return a;
+}
+class AwesomeBooks {
+  constructor(awesomeBooks = []) {
+    this.awesomeBooks = awesomeBooks;
+    this.id = 0;
+  }
+
+  addBook(Book) {
+    Book.id = this.id + 1;
+    this.id += 1;
+
+    this.awesomeBooks.push(Book);
+
+    const singleBook = document.createElement('div');
+    singleBook.classList.add('single-book');
+    singleBook.id = `Book${Book.id}`;
+    books.appendChild(singleBook);
+
+    const titleDiv = document.createElement('div');
+    titleDiv.textContent = Book.title;
+    singleBook.appendChild(titleDiv);
+
+    const authorDiv = document.createElement('div');
+    authorDiv.textContent = Book.author;
+    singleBook.appendChild(authorDiv);
+
+    const buttonDiv = document.createElement('button');
+    buttonDiv.textContent = 'Remove';
+    buttonDiv.id = `button_${Book.id}`;
+    buttonDiv.onclick = function () {
+      deleteBook(buttonDiv.id);
+    };
+    singleBook.appendChild(buttonDiv);
+    //updateLocalStorage();
+  }
+
+  removeBook(id) {
+    this.awesomeBooks = this.awesomeBooks.filter((book) => book.id.toString() !== id.toString());
+    document.querySelector(`#Book${id}`).remove();
+    //updateLocalStorage();
+  }
+}
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+    this.id = 0;
+  }
+}
+
+const myLibrary = new AwesomeBooks();
+
+/*
 let awesomeBooks = [];
 const awesomeBooks2 = JSON.parse(localStorage.getItem('awesomeBooks')) || [];
 let id = 0;
@@ -50,18 +108,12 @@ function addBook(title, author) {
   singleBook.appendChild(buttonDiv);
   updateLocalStorage();
 }
+*/
 
 const addButton = document.querySelector('#add-button');
 addButton.addEventListener('click', () => {
-  addBook(
-    document.querySelector('#title').value,
-    document.querySelector('#author').value,
-  );
+  const newBook = new Book(document.querySelector('#title').value, document.querySelector('#author').value);
+  myLibrary.addBook(newBook);
+  console.log(myLibrary);
 });
 
-function renderBooks() {
-  awesomeBooks2.forEach((book) => {
-    addBook(book.title, book.author);
-  });
-}
-renderBooks();
